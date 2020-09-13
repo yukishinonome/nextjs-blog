8fff0e9417b6bd74030408229014aab6ef20769d
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-09-13 21:22:19 +0900

    lesson 8

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,10 +3,16 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Yuki Shinonome'
+const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
